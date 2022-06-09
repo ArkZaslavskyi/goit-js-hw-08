c10b9066bbb89eb0a958118eea718d6bac8a34e7
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-06-10 02:13:00 +0300

    task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// 
+const galleryRef = document.querySelector('.gallery');
+
+// 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// 
+createGallery();
+
+// const lightbox =
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250' });
+	
+// 
+function createGallery() {
+    const galleryMarkup = galleryItems.map(item => createGaleryItemMarkup(item)).join('');
+
+    galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+};
+
+// 
+function createGaleryItemMarkup({ preview, original, description }) {
+    return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
+const LOCALSTORAGE_KEY_PLAYER_TIME = 'videoplayer-current-time';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+    console.log('played the video!');
+});
+
+player.on('timeupdate',
+    _.throttle(
+        function (data) {
+    console.log('seconds:', data.seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+        }
+        , 1000)
+);
+
+const persistedVideoTime = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY_PLAYER_TIME));
+
+if (persistedVideoTime) {
+    console.log('persistedVideoTime: ', persistedVideoTime);
+    player.setCurrentTime(persistedVideoTime);
+/*
+        .then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+    */
+}
+
+player.play();
+
 
